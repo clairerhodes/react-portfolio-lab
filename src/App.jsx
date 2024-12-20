21f093be83e82fa84c4636ae75d91cdf1a2d8373
Claire Rhodes <claire.rhodes13@gmail.com>
2024-12-20 00:15:49 -0800

    starting bio

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const mySkills = [
+    {
+      name: "Manager",
+      company: "",
+      comfort: ,
+      timeSpent: "2.5 years", 
+    }
+  ]
+
+  return (
+    <>
+      <h1>Claire Rhodes</h1>
+      <h4>Aspiring Software Developer</h4>
+      <p>Slay pookianna!</p>
+    </>
+  )
+}
+
+export default App
